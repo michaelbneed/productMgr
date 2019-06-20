d7bfd605e96d9ad1a7b2c4c47833acfed1d924ce
Doug Whitlock <doug@dougwhitlock.com>
2019-06-20 11:15:09 -0400

    Added method and settings for sending emails to admin/vendor users.

--- PM.Database/Email/Request.cs
+++ PM.Database/Email/Request.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using PM.Auth.GraphApi;
+using PM.Entity.Services;
 
 namespace PM.Business.Email
 {
@@ -6,9 +9,114 @@ namespace PM.Business.Email
     {
         private readonly IConfiguration _configuration;
 
-        public Request(IConfiguration configuration)
+        private readonly IDbReadService _dbReadService;
+
+        public Request(IConfiguration configuration, IDbReadService dbReadService)
         {
             _configuration = configuration;
+            _dbReadService = dbReadService;
+        }
+
+        public void SendNewRequestToHeadQuarters(Entity.Models.Request request)
+        {
+            var adminUrl = _configuration.GetValue<string>("AdminWebsite:BaseUrl");
+            var requestEditPath = string.Format(_configuration.GetValue<string>("AdminWebsite:RequestEdit"), request.Id);
+            var subject = $"Vandivier Product Request";
+            var emailAddresses = new GraphClient(_configuration, false).GetGroupUsersEmail(_configuration.GetValue<string>("SecurityGroups:HeadQuarters"));
+
+            foreach (var emailAddress in emailAddresses)
+            {
+                var body = $"A new product request has been added that requires your attention. <br /><br />" +
+                           $"View the request <a href='{adminUrl}{requestEditPath}'>here</a> <br /><br />" +
+                           $"Thanks, <br /> Vandivier Management";
+
+                Helper.Send(_configuration, subject, body, new List<string>() { emailAddress });
+            }
+        }
+
+        public async void SendRequestToSuppliers(Entity.Models.Request request)
+        {
+            var adminUrl = _configuration.GetValue<string>("AdminWebsite:BaseUrl");
+            var requestEditPath = string.Format(_configuration.GetValue<string>("AdminWebsite:RequestEdit"), request.Id);
+            var subject = $"Vandivier Product Request";
+            var users = await _dbReadService.GetAllRecordsAsync<Entity.Models.User>(x => x.SupplierId == request.SupplierId.Value);
+
+            foreach (var user in users)
+            {
+                var body = $"A new product request has been added that requires your attention. <br /><br />" +
+                           $"View the request <a href='{adminUrl}{requestEditPath}'>here</a> <br /><br />" +
+                           $"Thanks, <br /> Vandivier Management";
+
+                Helper.Send(_configuration, subject, body, new List<string>() { user.EmailAddress });
+            }
+        }
+
+        public void SendNewNoteEmailToHeadQuarters(Entity.Models.Request request, Entity.Models.Note note)
+        {
+            var adminUrl = _configuration.GetValue<string>("AdminWebsite:BaseUrl");
+            var noteDetailPath = string.Format(_configuration.GetValue<string>("AdminWebsite:NoteDetail"), note.Id);
+            var subject = $"Vandivier Product Request Note";
+            var emailAddresses = new GraphClient(_configuration, false).GetGroupUsersEmail(_configuration.GetValue<string>("SecurityGroups:HeadQuarters"));
+
+            foreach (var emailAddress in emailAddresses)
+            {
+                var body = $"A new note has been added for a product request that requires your attention. <br /><br />" +
+                           $"View the note <a href='{adminUrl}{noteDetailPath}'>here</a> <br /><br />" +
+                           $"Thanks, <br /> Vandivier Management";
+
+                Helper.Send(_configuration, subject, body, new List<string>() { emailAddress });
+            }
+        }
+
+        public void SendNewNoteEmailToStoreManagers(Entity.Models.Request request, Entity.Models.Note note)
+        {
+            var adminUrl = _configuration.GetValue<string>("AdminWebsite:BaseUrl");
+            var noteDetailPath = string.Format(_configuration.GetValue<string>("AdminWebsite:NoteDetail"), note.Id);
+            var subject = $"Vandivier Product Request Note";
+            var emailAddresses = new GraphClient(_configuration, false).GetGroupUsersEmail(_configuration.GetValue<string>("SecurityGroups:StoreManager"));
+
+            foreach (var emailAddress in emailAddresses)
+            {
+                var body = $"A new note has been added for a product request that requires your attention. <br /><br />" +
+                           $"View the note <a href='{adminUrl}{noteDetailPath}'>here</a> <br /><br />" +
+                           $"Thanks, <br /> Vandivier Management";
+
+                Helper.Send(_configuration, subject, body, new List<string>() { emailAddress });
+            }
+        }
+
+        public void SendNewNoteEmailToOriginatingUser(Entity.Models.Request request, Entity.Models.Note note)
+        {
+            // TODO: UserId needs to be a string.
+            var adminUrl = _configuration.GetValue<string>("AdminWebsite:BaseUrl");
+            var noteDetailPath = string.Format(_configuration.GetValue<string>("AdminWebsite:NoteDetail"), note.Id);
+            var subject = $"Vandivier Product Request Note";
+            var emailAddress = new GraphClient(_configuration, false).GetUserEmail(request.UserId.ToString());
+            var body = $"A new note has been added for a product request that requires your attention. <br /><br />" +
+                        $"View the note <a href='{adminUrl}{noteDetailPath}'>here</a> <br /><br />" +
+                        $"Thanks, <br /> Vandivier Management";
+
+            Helper.Send(_configuration, subject, body, new List<string>() { emailAddress });
+        }
+
+        public async void SendNewNoteEmailToSuppliers(Entity.Models.Request request, Entity.Models.Note note)
+        {
+            if (request.SupplierId.HasValue == false)
+                return;
+
+            var vendorUrl = _configuration.GetValue<string>("VendorWebsite:BaseUrl");
+            var noteDetailPath = string.Format(_configuration.GetValue<string>("VendorWebsite:NoteDetail"), note.Id);
+            var subject = $"Vandivier Product Request Note";
+            var users = await _dbReadService.GetAllRecordsAsync<Entity.Models.User>(x => x.SupplierId == request.SupplierId.Value);
+
+            foreach (var user in users)
+            {
+                var body = $"A new note has been added for a product request that requires your attention. <br /><br />" +
+                           $"View the note <a href='{vendorUrl}{noteDetailPath}'>here</a> <br /><br />" +
+                           $"Thanks, <br /> Vandivier Management";
+
+                Helper.Send(_configuration, subject, body, new List<string>() { user.EmailAddress });
+            }
         }
     }
 }
--- PM.Database/Email/User.cs
+++ PM.Database/Email/User.cs
@@ -14,7 +14,7 @@ namespace PM.Business.Email
 
         public void SendWelcomeEmail(PM.Entity.Models.User user, string password)
         {
-            var vendorUrl = _configuration.GetValue<string>("VendorWebsite");
+            var vendorUrl = _configuration.GetValue<string>("VendorWebsite:BaseUrl");
             var subject = "Welcome to Vandivier's Product Manager Platform";
             var body = $"Hi {user.FirstName} {user.LastName}, <br /><br /> Welcome to our product management platform, below you will find your username and password. <br /><br /><b>Username:</b> {user.EmailAddress} <br /><br /><b>Password:</b> {password} <br /><br />You can access the site <a href='{vendorUrl}'>here</a> <br /><br /> Thanks, <br /> Vandivier Management";
 
