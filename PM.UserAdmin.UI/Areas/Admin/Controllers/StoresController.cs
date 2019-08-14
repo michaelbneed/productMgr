2ed009188395ee8ef479d813ca8447c78c6535d4
mneed <mneed@adaptivesys.com>
2019-08-14 14:16:57 -0400

    Controller and views for DDL management (Stores). Added menu item to Layout.

---
+++ PM.UserAdmin.UI/Areas/Admin/Controllers/StoresController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PM.Entity.Models;
+using PM.Entity.Services;
+
+namespace PM.UserAdmin.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class StoresController : Controller
+    {
+	    private readonly IDbReadService _dbReadService;
+	    private readonly IDbWriteService _dbWriteService;
+
+		public StoresController(IDbReadService dbReadService, IDbWriteService dbWriteService)
+		{
+			_dbReadService = dbReadService;
+			_dbWriteService = dbWriteService;
+		}
+
+		public async Task<IActionResult> Index()
+        {
+	        var stores = await _dbReadService.GetAllRecordsAsync<Store>();
+	        return View(stores.OrderBy(s => s.StoreName));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+			var store = await _dbReadService.GetSingleRecordAsync<Store>(u => u.Id.Equals(id));
+
+			if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,StoreName,StoreSupervisorName,StoreSupervisorEmail,CreatedOn,CreatedBy,UpdatedOn,UpdatedBy")] Store store)
+        {
+            if (ModelState.IsValid)
+            {
+	            if (User != null)
+	            {
+		            var userFullName = User.Claims.FirstOrDefault(x => x.Type == $"name").Value;
+		            store.CreatedBy = userFullName;
+	            }
+
+	            store.CreatedOn = DateTime.Now;
+
+	            _dbWriteService.Add(store);
+
+	            await _dbWriteService.SaveChangesAsync();
+
+	            return RedirectToAction(nameof(Index));
+			}
+            return View(store);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+			var store = await _dbReadService.GetSingleRecordAsync<Store>(s => s.Id.Equals(id));
+
+			if (store == null)
+            {
+                return NotFound();
+            }
+            return View(store);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,StoreName,StoreSupervisorName,StoreSupervisorEmail,CreatedOn,CreatedBy,UpdatedOn,UpdatedBy")] Store store)
+        {
+            if (id != store.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+				try
+				{
+					if (User != null)
+					{
+						var userFullName = User.Claims.FirstOrDefault(x => x.Type == $"name").Value;
+						store.UpdatedBy = userFullName;
+					}
+
+					store.UpdatedOn = DateTime.Now;
+
+					_dbWriteService.Update(store);
+
+					await _dbWriteService.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+					bool result = await StoreExists(store.Id);
+					if (!result)
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+                return RedirectToAction(nameof(Index));
+            }
+            return View(store);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+			var store = await _dbReadService.GetSingleRecordAsync<Store>(s => s.Id.Equals(id));
+
+			if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        // POST: Admin/Stores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+			var store = await _dbReadService.GetSingleRecordAsync<Store>(s => s.Id.Equals(id));
+
+			_dbWriteService.Delete(store);
+
+			var response = await _dbWriteService.SaveChangesAsync();
+
+			if (!response)
+			{
+				TempData["notifyUser"] = "This action could not be performed due to data constraints.";
+			}
+
+			return RedirectToAction(nameof(Index));
+		}
+
+		private async Task<bool> StoreExists(int id)
+		{
+			var store = _dbReadService.GetSingleRecordAsync<Store>(s => s.Id.Equals(id));
+			return await _dbReadService.DoesRecordExist<Store>(e => store.Id == id);
+		}
+	}
+}
